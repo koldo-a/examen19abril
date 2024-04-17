358e440b73057d4f33cc23db9e30681d1b7a2352
JAVA <JAVA@192.168.0.74>
2024-04-17 12:59:26 +0200

    First Commit

---
+++ src/main/java/com/ipartek/formacion/uf2218/ejemplos/HolaMundo.java
@@ -0,0 +1,21 @@
+package com.ipartek.formacion.uf2218.ejemplos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hola")
+public class HolaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Hola");
+	}
+
+}
---
+++ src/main/webapp/ejemplo.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Prueba</title>
+</head>
+<body>
+	<%
+	for (int i = 1; i < 6; i++) {
+	%>
+	<h1>Prueba <%= i %></h1>
+	<%
+	}
+	%>
+</body>
+</html>
